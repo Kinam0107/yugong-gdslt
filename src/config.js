8c50d43990573cff290ff082398d82d9c78f2b67
吴奇楠 <kinam5@163.com>
2024-02-28 11:14:47 +0800

    feat: base map function

--- src/config.js
+++ src/config.js
@@ -1,3 +1,7 @@
+import provinceLevelBoundary from './assets/geoJson/gdProvinceGeoJson.json'
+import cityLevelBoundary from './assets/geoJson/gdCityGeoJson.json'
+import villageLevelBoundary from './assets/geoJson/gdVillageGeoJson.json'
+
 const servers = {
   线上环境: 'http://192.168.2.144:33444/api',
   路亚峰: 'http://192.168.2.111:15001',
@@ -7,9 +11,19 @@ const servers = {
 }
 
 export default {
+  proxyTarget: servers['线上环境'],
   baseUrl: '/api',
   timeout: undefined,
-  proxyTarget: servers['线上环境'],
   disablePermit: false,
-  bypassLogin: false
+  bypassLogin: false,
+  map: {
+    secretKey: 'ed01c047277d7afe209a42a4c9ce4eda',
+    center: [113.43, 22.87],
+    defaultZoom: 7.4,
+    maxZoom: 16,
+    minZoom: 5,
+    provinceLevelBoundary,
+    cityLevelBoundary,
+    villageLevelBoundary
+  }
 }
