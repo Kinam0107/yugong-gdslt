817fcc3f9489a3dc489ea1d0e24f980d1531682b
吴奇楠 <kinam5@163.com>
2024-11-20 16:31:48 +0800

    feat:三区三线地图浮窗

--- src/components/map/AuxiliaryInfo.vue
+++ src/components/map/AuxiliaryInfo.vue
@@ -26,6 +26,65 @@
       <i class="cube" style="background-image: linear-gradient(180deg, #0453de 0%, #fffe39 27%, #ff9e24 59%, #fc0106 100%)"></i>
       <span>洪水风险图</span>
     </el-checkbox>
+    <div ref="featureOverlay">
+      <template v-if="showFeatureOverlay">
+        <PopupBox @close="closeFeatureOverlay">
+          <template v-if="featureOverlayData.GHFQMC">
+            <div class="item">
+              <span class="label">规划分区：</span>
+              <span class="value">{{ featureOverlayData.GHFQMC || '-' }}</span>
+            </div>
+            <div class="item">
+              <span class="label">面积：</span>
+              <span class="value">{{ featureOverlayData.MJ || '-' }}m²</span>
+            </div>
+          </template>
+          <template v-else-if="featureOverlayData.DLMC">
+            <div class="item">
+              <span class="label">地类：</span>
+              <span class="value">{{ featureOverlayData.DLMC || '-' }}</span>
+            </div>
+            <div class="item">
+              <span class="label">权属单位：</span>
+              <span class="value">{{ featureOverlayData.QSDWMC || '-' }}</span>
+            </div>
+            <div class="item">
+              <span class="label">永久基本农田图斑面积：</span>
+              <span class="value">{{ featureOverlayData.YJJBNTTBMJ || '-' }}</span>
+            </div>
+            <div class="item">
+              <span class="label">扣除面积：</span>
+              <span class="value">{{ featureOverlayData.KCMJ || '-' }}</span>
+            </div>
+            <div class="item">
+              <span class="label">永久基本农田面积：</span>
+              <span class="value">{{ featureOverlayData.YJJBNTMJ || '-' }}</span>
+            </div>
+            <div class="item">
+              <span class="label">耕地类型：</span>
+              <span class="value">{{ featureOverlayData.GDLX || '-' }}</span>
+            </div>
+            <div class="item">
+              <span class="label">种植属性：</span>
+              <span class="value">{{ featureOverlayData.ZZSXMC || '-' }}</span>
+            </div>
+          </template>
+          <template v-else-if="featureOverlayData.HXMC">
+            <div class="item">
+              <span class="value">{{ featureOverlayData.HXMC || '-' }}</span>
+            </div>
+            <div class="item">
+              <span class="label">红线类型：</span>
+              <span class="value">{{ featureOverlayData.HXLX || '-' }}</span>
+            </div>
+            <div class="item">
+              <span class="label">面积：</span>
+              <span class="value">{{ featureOverlayData.MJ || '-' }}m²</span>
+            </div>
+          </template>
+        </PopupBox>
+      </template>
+    </div>
   </div>
 </template>
 
@@ -34,6 +93,8 @@ import { onMounted, ref } from 'vue'
 import axios from 'axios'
 import Image from 'ol/layer/Image'
 import ImageWMS from 'ol/source/ImageWMS'
+import PopupBox from '@/components/map/PopupBox.vue'
+import { renderPositionOverlay, removeLayer } from '@/utils/map'
 
 const props = defineProps({
   map: {
@@ -41,19 +102,42 @@ const props = defineProps({
   }
 })
 
+const featureOverlay = ref()
+const showFeatureOverlay = ref(false)
+const featureOverlayData = ref({})
+const openFeatureOverlay = (data, coordinate) => {
+  featureOverlayData.value = data
+  showFeatureOverlay.value = true
+  renderPositionOverlay(props.map, '区域信息', { coordinate }, featureOverlay.value)
+}
+const closeFeatureOverlay = () => {
+  featureOverlayData.value = {}
+  showFeatureOverlay.value = false
+  removeLayer(props.map, '区域信息')
+}
 onMounted(() => {
   props.map.on('singleclick', (event) => {
+    const feature = props.map.forEachFeatureAtPixel(event.pixel, (feat, layer) => {
+      if (layer?.get('layerName')) {
+        return feat
+      } else if (layer?.get('clusterLayerName') && feat.get('features').length == 1) {
+        return feat.get('features')[0]
+      }
+    })
+    if (feature) {
+      closeFeatureOverlay()
+      return
+    }
     const viewResolution = props.map.getView().getResolution()
-    const url = sqsxLayer.getSource().getFeatureInfoUrl(
-      event.coordinate,
-      viewResolution,
-      'EPSG:4326',
-      { INFO_FORMAT: 'application/geojson' } // 或者 'text/html' 或 'text/plain'
-    )
+    const url = sqsxLayer.getSource().getFeatureInfoUrl(event.coordinate, viewResolution, 'EPSG:4326', { INFO_FORMAT: 'application/geojson' })
     if (url) {
       axios.get(url).then((res) => {
         const properties = res.data?.features?.[0]?.properties
-        console.log(properties)
+        if (properties) {
+          openFeatureOverlay(properties, event.coordinate)
+        } else {
+          closeFeatureOverlay()
+        }
       })
     }
   })
